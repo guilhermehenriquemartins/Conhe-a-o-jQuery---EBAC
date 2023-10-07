554abb3d12c5a4ed2dba8132be4a7015e67e5bc4
Guilherme Henrique <88465375+guilhermehenriquemartins@users.noreply.github.com>
2023-10-07 13:48:40 -0300

    Revisão Aulas jQuery

--- programando.js
+++ programando.js
@@ -1,3 +1,13 @@
 $(document).ready(function() {
-  $('header button')
+  $('#botao-adicionar').click(function(e) {
+    e.preventDefault();
+  })
+
+  $('header button').click(function() {
+    $('form').slideDown();
+  })
+
+  $('#botao-cancelar').click(function() {
+    $('form').slideUp();
+  })
 })

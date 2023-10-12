53ffcd1bd6d15d8046550aa192126c0ca895d044
Guilherme Henrique <88465375+guilhermehenriquemartins@users.noreply.github.com>
2023-10-12 19:19:09 -0300

    Aula 04 Concluída

--- programando.js
+++ programando.js
@@ -1,13 +1,22 @@
 $(document).ready(function() {
-  $('#botao-adicionar').click(function(e) {
-    e.preventDefault();
-  })
-
   $('header button').click(function() {
     $('form').slideDown();
   })
 
   $('#botao-cancelar').click(function() {
     $('form').slideUp();
+  })
+
+  $('form').on('submit', function(e){
+    e.preventDefault();
+    const EnderecoDaImagem = $('#endereco-imagem').val();
+    const NovaImagem = $('<li style="display:none"></li>');
+    $(`<img src="${EnderecoDaImagem}"/>`).appendTo(NovaImagem)
+    $(`<div class="overflow-imagem-link">
+    <a href="${EnderecoDaImagem}" title="Ver imagem em tamanho real" target="_blank"> Ver imagem em tamanho real</a>
+      </div>`).appendTo(NovaImagem)
+    $(NovaImagem).appendTo('ul')
+    $(NovaImagem).fadeIn(1000)
+    $('#endereco-imagem').val('')
   })
 })
